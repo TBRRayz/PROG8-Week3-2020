8cc30fee9c1c23d30185077a08191b2ed4e97798
Bryan <bryanderonde@hotmail.com>
2020-05-21 12:43:20 +0200

    make the variable of the ball class public because we need then in the bouncing and space class, and remove the abstract property so we can call the ball class form the main class

--- dev/ball.ts
+++ dev/ball.ts
@@ -1,21 +1,26 @@
-abstract class Ball extends HTMLElement{
+class Ball extends HTMLElement{
 
-    protected readonly gravity    : number = 0.1
-    protected readonly friction   : number = 0.9
+    public readonly gravity    : number = 0.1
+    public readonly friction   : number = 0.9
 
-    protected x           : number = 0
-    protected y           : number = 0
-    protected speedX      : number = 5
-    protected speedY      : number = -3
-    protected minWidth    : number = 0
-    protected maxWidth    : number = 0
-    protected maxHeight   : number = 0
+    public x           : number = 0
+    public y           : number = 0
+    public speedX      : number = 5
+    public speedY      : number = -3
+    public minWidth    : number = 0
+    public maxWidth    : number = 0
+    public maxHeight   : number = 0
 
+    private ballBehavior : BallBehavior;
+
+    public setBehavior(behavior: BallBehavior) {
+        this.ballBehavior = behavior;
+    }
     
-    public get X() : number { return this.x }
-    
-    constructor(minWidth : number, maxWidth : number) {
+    constructor(minWidth : number, maxWidth : number, behavior : BallBehavior) {
         super()
+
+        this.ballBehavior = behavior;
 
         let content = document.getElementsByTagName("content")[0]
         content.appendChild(this)
