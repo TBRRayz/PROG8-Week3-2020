1ebe84ab6417e79ee626e53438c6c6a6c3e5e629
Bryan <bryanderonde@hotmail.com>
2020-05-21 12:45:57 +0200

    change the balls in main to al ball component istead of a moonball, earthball or a basketbal but give them a behavior

--- dev/main.ts
+++ dev/main.ts
@@ -1,14 +1,14 @@
 class Main {
 
     private balls : Ball[] = []
-    private basketBall : BasketBall
+    private basketBall : Ball;
 
     constructor() {
         
-        this.balls.push(new EarthBall(0, window.innerWidth/2))
-        this.balls.push(new MoonBall(window.innerWidth/2, window.innerWidth))
+        this.balls.push(new Ball(0, window.innerWidth/2, new Bouncing()))
+        this.balls.push(new Ball(window.innerWidth/2, window.innerWidth, new Space()))
 
-        this.basketBall = new BasketBall(0, window.innerWidth)
+        this.basketBall = new Ball(0, window.innerWidth, new Bouncing);
         this.gameLoop()
     }
 
